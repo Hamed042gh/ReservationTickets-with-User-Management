f9538a20308cab7beaec4ef4c88017c76f90b98d
Hamed042gh <blearn704@gmail.com>
2024-10-31 13:53:59 +0330

    Feat:Redis Lock Implementation for Reservations - Installed and configured Redis for efficient caching. - Implemented locks to prevent issues with high reservation requests and ensure data integrity.

--- app/Livewire/ReservationTickets.php
+++ app/Livewire/ReservationTickets.php
@@ -7,6 +7,7 @@ use Livewire\Component;
 use App\Models\Reservation;
 use Livewire\Attributes\On;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redis;
 
 class ReservationTickets extends Component
 {
@@ -15,7 +16,7 @@ class ReservationTickets extends Component
     public $previewReservation = false;
     public $selectedTicket;
     public $reservationData = [];
-
+    private $lockKey;
 
     public function mount()
     {
@@ -29,7 +30,10 @@ class ReservationTickets extends Component
         if (!$this->user) {
             return redirect('/login');
         }
+
+
         $ticket = Ticket::findOrFail($ticketId);
+
         if (($ticket->available_count) < 1) {
             $this->dispatch('showError', 'No available seats for this ticket!');
             return;
@@ -49,6 +53,11 @@ class ReservationTickets extends Component
     {
         $ticket = $this->selectedTicket;
 
+        if (!$this->setRedisLock($ticket)) {
+
+            return redirect()->back()->with('error', 'This ticket is currently being reserved by someone else. Please try again later.');
+        }
+
         if ($ticket && ($ticket->available_count) >= 1) {
 
 
@@ -58,9 +67,10 @@ class ReservationTickets extends Component
                 'reservation_date' => $ticket->departure_date,
             ]);
             $this->resetPreview();
-
+            Redis::del($this->lockKey);
             return redirect()->route('purchase', ['ticket' => $ticket]);
         } else {
+            Redis::del($this->lockKey);
             return redirect()->back()->with('error', 'No available seats for this ticket!'); // Error message
         }
     }
@@ -73,6 +83,12 @@ class ReservationTickets extends Component
         session()->flash('message', 'Reservation canceled!');
     }
 
+    private function setRedisLock($ticket)
+    {
+
+        $lockKey = 'ticket_lock_' . $ticket->id;
+        $lock = Redis::set($lockKey, 1, 'NX', 'EX', 10);
+    }
     public function render()
     {
         return view('livewire.reservation-tickets');
