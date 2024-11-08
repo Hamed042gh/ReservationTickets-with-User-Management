867fff63f728095e303d6b22839d0a1701034325
Hamed042gh <blearn704@gmail.com>
2024-11-08 09:53:31 +0330

    feat:Add UpdateTicketsCount event for real-time ticket updates

---
+++ app/Events/UpdateTicketsCount.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class UpdateTicketsCount implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $user;
+    public $ticket;
+
+    public function __construct($user, $ticket)
+    {
+        $this->user = $user;
+        $this->ticket = $ticket;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastWith()
+    {
+        return [
+
+            'origin' => $this->ticket['origin'],
+            'destination' => $this->ticket['destination'],
+            'departure' => $this->ticket['departure'],
+            'available_count' => $this->ticket['available_count'],
+        ];
+    }
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel('tickets'),
+        ];
+    }
+}
