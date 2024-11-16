57201885957571e9b245e946bcf5719a91b7046c
Hamed042gh <blearn704@gmail.com>
2024-11-16 13:22:30 +0330

    refactor: Optimize route file for better maintainability

--- routes/web.php
+++ routes/web.php
@@ -12,33 +12,49 @@ Route::get('/', function () {
     });
 });
 
-
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
-
-require __DIR__ . '/auth.php';
-
-Broadcast::routes(['middleware' => ['web', 'auth']]);
-
-Route::middleware(['auth'])->group(function () {
-
-    Route::get('/purchase{ticket}', [TicketController::class, 'purchase'])->middleware('Check.Reservation')->name('purchase');
-    Route::get('/cancelpurchase{ticket}', [TicketController::class, 'cancelPurchase'])->middleware('Check.Reservation')->name('cancelPurchase');
-    Route::get('/reservations', [UserReservationsController::class, 'showReservations'])->name('reservations');
-    Route::get('/finance', [UserReservationsController::class, 'showFinance'])->name('finance');
-    Route::delete('reservations/delete/{id}', [UserReservationsController::class, 'deleteReservation'])->name('deleteReservation');
-
-});
+// general
 Route::get('/tickets', [TicketController::class, 'showTickets'])->name('tickets');
 
+// group with autentication
 Route::middleware(['auth'])->group(function () {
-    Route::post('/payment/request', [PaymentController::class, 'requestPayment']);
-    Route::get('/payment/callback', [PaymentController::class, 'verifyPayment']);
-    Route::post('/payment/verify', [PaymentController::class, 'verifyPayment']);
-    Route::post('/payment/inquiry', [PaymentController::class, 'inquiryPayment'])->name('payment.inquiry');
+
+    // users
+    Route::view('dashboard', 'dashboard')
+        ->middleware('verified')
+        ->name('dashboard');
+
+    Route::view('profile', 'profile')
+        ->name('profile');
+
+    // Reservations & Payments
+    Route::get('/purchase/{ticket}', [TicketController::class, 'purchase'])
+        ->middleware('Check.Reservation')
+        ->name('purchase');
+
+    Route::get('/cancelpurchase/{ticket}', [TicketController::class, 'cancelPurchase'])
+        ->middleware('Check.Reservation')
+        ->name('cancelPurchase');
+
+    Route::get('/reservations', [UserReservationsController::class, 'showReservations'])
+        ->name('reservations');
+
+    Route::get('/finance', [UserReservationsController::class, 'showFinance'])
+        ->name('finance');
+
+    Route::delete('reservations/delete/{id}', [UserReservationsController::class, 'deleteReservation'])
+        ->name('deleteReservation');
+
+    // payments
+    Route::prefix('payment')->group(function () {
+        Route::post('/request', [PaymentController::class, 'requestPayment']);
+        Route::get('/callback', [PaymentController::class, 'verifyPayment']);
+        Route::post('/verify', [PaymentController::class, 'verifyPayment']);
+        Route::post('/inquiry', [PaymentController::class, 'inquiryPayment'])->name('payment.inquiry');
+    });
 });
+
+// Autentication
+require __DIR__ . '/auth.php';
+
+// Broadcast routes
+Broadcast::routes(['middleware' => ['web', 'auth']]);
